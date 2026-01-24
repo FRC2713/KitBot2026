631603f2ba2bec1544b9d8be4c36b79f1f81ed0f
carrotfarm02 <wyattalanbach@gmail.com>
2026-01-23 19:48:47 -0500

    added further PID configuration

--- src/main/java/frc/robot/subsystems/prototypeLauncher/PrototypeLauncher.java
+++ src/main/java/frc/robot/subsystems/prototypeLauncher/PrototypeLauncher.java
@@ -1,20 +1,17 @@
 package frc.robot.subsystems.prototypeLauncher;
 
-import com.ctre.phoenix6.configs.ClosedLoopGeneralConfigs;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
 import edu.wpi.first.units.measure.AngularVelocity;
-import edu.wpi.first.units.measure.Velocity;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-import static edu.wpi.first.units.Units.Rotations;
-import static edu.wpi.first.units.Units.RotationsPerSecond;
-
+import frc.robot.util.LoggedTunableNumber;
 import org.littletonrobotics.junction.Logger;
 
 public class PrototypeLauncher extends SubsystemBase {
@@ -23,9 +20,13 @@ public class PrototypeLauncher extends SubsystemBase {
   private double trackedvoltage = 0;
   private AngularVelocity velocity = RotationsPerSecond.of(0);
   private TalonFXConfiguration leftMotorConfig = new TalonFXConfiguration();
+  private LoggedTunableNumber prototypeP = new LoggedTunableNumber("prototypeP", 0);
 
   @Override
   public void periodic() {
+    if (prototypeP.hasChanged(hashCode())) {
+      this.setPID(prototypeP.get());
+    }
     Logger.recordOutput("Prototype/voltage", trackedvoltage);
     Logger.recordOutput("Prototype/desiredVelocity", velocity);
     Logger.recordOutput("Prototype/velocity", leftMotor.getVelocity().getValue());
@@ -33,7 +34,11 @@ public class PrototypeLauncher extends SubsystemBase {
 
   public PrototypeLauncher() {
     rightMotor.setControl(new Follower(leftMotor.getDeviceID(), MotorAlignmentValue.Opposed));
-    leftMotorConfig.Slot0.kP = 0.01;
+    this.setPID(prototypeP.get());
+  }
+
+  public void setPID(double P) {
+    leftMotorConfig.Slot0.kP = P;
     leftMotorConfig.Slot0.kV = 500;
     leftMotor.getConfigurator().apply(leftMotorConfig);
   }
